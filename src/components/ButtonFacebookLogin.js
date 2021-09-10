f806ad0d7b977ba7292e241ff73caebbc167f415
João Victor <jovic.morais@unifesspa.edu.br>
2021-09-10 12:00:47 -0300

    remoção de importações inúteis

--- src/components/ButtonFacebookLogin.js
+++ src/components/ButtonFacebookLogin.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import Button from './Button';
+import {StyleSheet, Text} from 'react-native';
 import {theme} from './../global/styles/theme';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
