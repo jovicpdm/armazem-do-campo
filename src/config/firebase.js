afb2d00f1a4827f2c393cfa11332e13a63ed665a
João Victor Morais <jovic182015@gmail.com>
2021-09-16 18:25:46 -0300

    configuração do firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBNbWZOOHepdE8ztLjTOrOFXBnd-IKkbeI',
+  authDomain: 'armazem-do-campo.firebaseapp.com',
+  projectId: 'armazem-do-campo',
+  storageBucket: 'armazem-do-campo.appspot.com',
+  messagingSenderId: '460505070295',
+  appId: '1:460505070295:web:331cc6de12a7c2ae4c4e92',
+  measurementId: 'G-BNN7DJGBSK',
+};
+
+export default firebaseConfig;
