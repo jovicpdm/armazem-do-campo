f1d15d3f37e24fed9ddf691c5637a80a7a005029
João Victor <jovic.morais@unifesspa.edu.br>
2021-09-17 16:56:24 -0300

    remoção de configuração incorreta do firebase

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from './screens/Login';
 import Register from './screens/Register';
