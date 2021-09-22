c40939841a7b03858be1ae5c9858a1fa6fcdcc97
santos-joao07 <santos.joao@unifesspa.edu.br>
2021-09-22 13:35:25 -0300

    adicionada dependências firebase

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,15 +1,15 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
 import Logo from '../components/Logo';
 import Input from '../components/Input';
 import ButtonPrimary from '../components/ButtonPrimary';
-import {theme} from '../global/styles/theme';
+import { theme } from '../global/styles/theme';
 import ButtonSecondary from '../components/ButtonSecondary';
 import WhiteArea from '../components/WhiteArea';
 import TitleSection from '../components/TitleSection';
 
-export function Login({navigation}) {
+export function Login({ navigation }) {
   const [email, onChangeEmail] = useState();
   const [password, onChangePassword] = useState();
 
@@ -22,11 +22,17 @@ export function Login({navigation}) {
           placeholder={'Email'}
           onChangeText={onChangeEmail}
           //react-native/no-inline-styles
-          style={{marginTop: 16}}
+          style={{ marginTop: 16 }}
         />
-        <Input placeholder={'Senha'} onChangeText={onChangePassword} />
-        <View style={{marginTop: 32}} />
-        <ButtonPrimary>ENTRAR</ButtonPrimary>
+        <Input placeholder={'Senha'}
+          onChangeText={onChangePassword}
+        />
+        <View style={{ marginTop: 32 }} />
+        <ButtonPrimary
+          onPress={() => {
+
+          }}
+        >ENTRAR</ButtonPrimary>
         <ButtonSecondary
           onPress={() => {
             navigation.navigate('Register');
