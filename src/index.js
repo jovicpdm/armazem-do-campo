9ea3dbcba72cb35a662a6de2da0d13463a2f368e
João Victor <jovic.morais@unifesspa.edu.br>
2021-09-23 12:50:23 -0300

    remoção de trecho de código defeituosa

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
+import {LogBox} from 'react-native';
+LogBox.ignoreWarnings([
+  'Warning: Async Storage has been extracted from react-native core',
+]);
 
 import {theme} from './global/styles/theme';
 import Router from './router';
