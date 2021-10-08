0294893ba6cd19b2a8d941bb89dfda015b5e41a1
santos-joao07 <santos.joao@unifesspa.edu.br>
2021-10-08 18:23:34 -0300

    criação da tela de cadastrar produto

--- src/screens/RegisterProduct.js
+++ src/screens/RegisterProduct.js
@@ -1,19 +1,210 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import {Badge} from 'react-native-paper';
-import {theme} from '../global/styles/theme';
+import { theme } from '../global/styles/theme';
+import WhiteArea from '../components/WhiteArea';
+import Input from '../components/Input';
+import Picker from 'react-native-universal-picker';
+import ButtonSecondary from '../components/ButtonSecondary';
+import ButtonPrimary from '../components/ButtonPrimary';
+import {getDatabase, ref, set, push} from 'firebase/database';
 
-const RegisterProduct = ({navigation}) => {
+export default function RegisterProduct({ navigation }) {
+  const [productName, setProductName] = useState();
+  const [price, setPrice] = useState();
+  const [placeOfSale, setPlaceOfSale] = useState();
+  const [description, setDescription] = useState();
+
+  const [categories, setCategories] = useState([{ description: 'BATATAS', id: 1 },
+  { description: 'FRUTAS IN NATURA E POLPAS', id: 2 }, { description: 'LEGUMES', id: 3 },
+  { description: 'DERIVADOS DA MANDIOCA', id: 4 }, { description: 'LEITE, QUEIJO E OUTROS DERIVADOS', id: 5 },
+  { description: 'VERDURAS', id: 6 }, { description: 'CARNES E OVOS', id: 7 }, { description: 'BEBIDAS', id: 8 },
+  { description: 'TEMPEROS', id: 9 }, { description: 'ÓLEOS', id: 10 }, { description: 'PÃES', id: 11 },
+  { description: 'BISCOITOS E SALGADOS', id: 12 }]);
+  const [selectedCategory, setSelectedCategory] = useState();
+
+  const [formsOfSale, setFormsOfSale] = useState([{ description: 'UNIDADE', id: 1 },
+  { description: 'KG', id: 2 }]);
+  const [selectedFormOfSale, setSelectedFormOfSale] = useState();
+
+  const [deliveryDay, setDeliveryDay] = useState([{ description: 'DOMINGO', id: 1 },
+  { description: 'SEGUNDA', id: 2 }, { description: 'TERÇA', id: 3 },
+  { description: 'QUARTA', id: 4 }, { description: 'QUINTA', id: 5 },
+  { description: 'SEXTA', id: 6 }, { description: 'SÁBADO', id: 7 }]);
+  const [selectedDeliveryDay, setSelectedDeliveryDay] = useState();
+
+  const writeProductData = () => {
+    const db = getDatabase();
+    const productRef = ref(db, 'products');
+    const newProductRef = push(productRef);
+        set(newProductRef, {
+          name: productName,
+          price: price,
+          placeOfSale: placeOfSale,
+          description: description,
+          category: selectedCategory,
+          formOfSale: selectedFormOfSale,
+          deliveryDay: selectedDeliveryDay,
+        });
+      }
+
   return (
     <View>
-      <Icon
-        name="plus-circle"
-        size={32}
-        color={theme.pallete.textTitleScreen}
-      />
+      <View style={styles.container}>
+        <Icon
+          name="plus-circle"
+          size={26}
+          color={theme.pallete.textTitleScreen}
+        />
+        <Text style={styles.addProduct}>
+          Cadastrar produto
+        </Text>
+      </View>
+      <WhiteArea>
+        <Input
+          placeholder="Nome do produto"
+          placeholderTextColor={theme.pallete.primary}
+          onChangeText={text => setProductName(text)}
+          value={productName}
+          keyboardType="default"
+          style={{ marginTop: 47 }}
+        />
+        <Input
+          placeholder="Preço"
+          placeholderTextColor={theme.pallete.primary}
+          onChangeText={text => setPrice(text)}
+          value={price}
+          keyboardType="numeric"
+          style={{ marginTop: 16 }}
+        />
+        <Input
+          placeholder="Local de venda"
+          placeholderTextColor={theme.pallete.primary}
+          onChangeText={text => setPlaceOfSale(text)}
+          value={placeOfSale}
+          keyboardType="default"
+          style={{ marginTop: 16 }}
+        />
+        <Input
+          placeholder="Descrição"
+          placeholderTextColor={theme.pallete.primary}
+          onChangeText={text => setDescription(text)}
+          value={description}
+          keyboardType="default"
+          style={styles.inputDescription}
+        />
+
+        <Text
+          style={styles.titlePicker}
+        >
+          Categoria</Text>
+        <View style={styles.select}>
+          <Picker
+            style={styles.txtPicker}
+            style={{ backgroundColor: theme.pallete.primary }}
+            selectedValue={selectedCategory}
+            style={{ height: "100%", width: "100%" }}
+            onValueChange={(itemValue, item) =>
+              setSelectedCategory(itemValue)
+            }>
+            {
+              categories.map(category => {
+                return <Picker.Item label={category.description} value={category.description} />
+              })
+            }
+          </Picker>
+        </View>
+
+        <Text
+          style={styles.titlePicker}
+        >
+          Forma de venda</Text>
+        <View style={styles.select}>
+          <Picker
+            style={styles.txtPicker}
+            selectedValue={selectedFormOfSale}
+            style={{ height: "100%", width: "100%" }}
+            onValueChange={(itemValue, item) =>
+              setSelectedFormOfSale(itemValue)
+            }>
+            {
+              formsOfSale.map(forms => {
+                return <Picker.Item label={forms.description} value={forms.description} />
+              })
+            }
+          </Picker>
+        </View>
+
+        <Text
+          style={styles.titlePicker}
+        >
+          Dia de entrega</Text>
+        <View style={styles.select}>
+          <Picker
+            style={styles.txtPicker}
+            selectedValue={selectedDeliveryDay}
+            style={{ height: "100%", width: "100%" }}
+            onValueChange={(itemValue, item) =>
+              setSelectedDeliveryDay(itemValue)
+            }>
+            {
+              deliveryDay.map(delivery => {
+                return <Picker.Item label={delivery.description} value={delivery.description} />
+              })
+            }
+          </Picker>
+        </View>
+
+        <View style={{marginTop: 30}} />
+        <ButtonPrimary onPress={() => writeProductData()}>CADASTRAR</ButtonPrimary>
+        <ButtonSecondary
+          onPress={() => {}}>
+          CANCELAR
+        </ButtonSecondary>
+
+      </WhiteArea>
     </View>
   );
 };
 
-export default RegisterProduct;
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  addProduct: {
+    fontFamily: 'Roboto-Medium',
+    fontSize: 24,
+    color: theme.pallete.white,
+    marginVertical: 24,
+    paddingHorizontal: 10,
+  },
+  inputDescription: {
+    paddingVertical: 60,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    marginTop: 16
+  },
+  titlePicker: {
+    marginTop: 5,
+    marginBottom: 2,
+    color: theme.pallete.primary,
+    fontSize: 16,
+  },
+  select: {
+    borderColor: theme.pallete.primary,
+    borderWidth: 1,
+    borderRadius: 8,
+    height: 30,
+    width: 230,
+    paddingHorizontal: 12,
+  },
+  txtPicker: {
+    borderEndColor: theme.pallete.primary,
+    tintColor: "#ffcbdb",
+    textDecorationColor: "#ffcbdb",
+    textShadowColor: '#ffcbdb',
+
+  },
+});
