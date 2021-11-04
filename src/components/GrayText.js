6e79257baddb7b817f0879c02716c39c777a1b9a
João Victor <jovic.morais@unifesspa.edu.br>
2021-11-04 10:46:28 -0300

    push de segurança

---
+++ src/components/GrayText.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {theme} from '../global/styles/theme';
+
+const GrayText = ({children}) => {
+  return <Text style={styles.text}>{children}</Text>;
+};
+
+export default GrayText;
+
+const styles = StyleSheet.create({
+  text: {
+    textAlign: 'auto',
+    marginTop: 8,
+    color: theme.pallete.gray001,
+    fontSize: 16,
+  },
+});
