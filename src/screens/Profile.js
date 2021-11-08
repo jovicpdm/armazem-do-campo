a1d754f6911f9c9e72b8f9b5be48ab27b688f21b
João Victor <jovic.morais@unifesspa.edu.br>
2021-11-08 12:40:22 -0300

    adicionado o loading em algumas telas

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {Avatar, AccessoryProps} from 'react-native-elements';
+import CardContainer from '../components/CardContainer';
 
-export class Profile {
-  render() {
-    return (
-      <View>
-        <Text>Profile</Text>
-      </View>
-    );
-  }
+import ProfilePhoto from '../components/ProfilePhoto';
+import TextCard from '../components/TextCard';
+import TitleScreen from '../components/TitleScreen';
+import TopScreen from '../components/TopScreen';
+import WhiteAreaWithoutScrollView from '../components/WhiteAreaWithoutScrollView';
+
+export default function Profile() {
+  return (
+    <View>
+      <TopScreen>
+        <TitleScreen>Perfil</TitleScreen>
+      </TopScreen>
+      <WhiteAreaWithoutScrollView>
+        <View style={{marginTop: 8}}>
+          <Avatar
+            rounded
+            size={64}
+            source={{
+              uri: 'https://emoff.ig.com.br/wp-content/uploads/2021/11/neymarjr-254529721_282681760417405_5748955508485612656_n-1.jpg',
+            }}
+            onPress={() => {
+              alert('clicou na imagem');
+            }}>
+            <Avatar.Accessory size={20} {...AccessoryProps} />
+          </Avatar>
+          <CardContainer />
+          <CardContainer>
+            <TextCard></TextCard>
+          </CardContainer>
+        </View>
+      </WhiteAreaWithoutScrollView>
+    </View>
+  );
 }
-
-export default Profile;
