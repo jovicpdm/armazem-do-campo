8fea6558e162e45a73f6feab1917e84e2d1b5b58
Ralfh <ralfhalan@gmail.com>
2022-08-05 13:36:12 -0300

    Correção de comentários

--- src/router.js
+++ src/router.js
@@ -12,11 +12,8 @@ import Admin from './screens/Admin';
 import ParticipantManagement from './screens/ParticipantManagement';
 import ProductManagement from './screens/ProductManagement';
 import ProductList from './screens/ProductList';
-<<<<<<< HEAD
 import EditProduct from './screens/EditProduct';
 import EditItem from './screens/EditItem';
-=======
->>>>>>> fd50340385659c05f908f8b2245fd9316331ddb4
 import ProductListDelete from './screens/ProductListDelete';
 import GeneralInformation from './screens/GeneralInformation';
 import Requests from './screens/Requests';
@@ -57,11 +54,8 @@ const Router = () => {
         />
         <Screen name="ProductManagement" component={ProductManagement} />
         <Screen name="ProductList" component={ProductList} />
-<<<<<<< HEAD
         <Screen name="EditProduct" component={EditProduct} />
         <Screen name="EditItem" component={EditItem} />
-=======
->>>>>>> fd50340385659c05f908f8b2245fd9316331ddb4
         <Screen name="ProductListDelete" component={ProductListDelete} />
         <Screen name="GeneralInformation" component={GeneralInformation} />
         <Screen name="Requests" component={Requests} />
