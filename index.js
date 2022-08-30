08e18b775391604014fabd60000707548344bb5f
Ralfh <ralfhalan@gmail.com>
2022-08-30 11:49:19 -0300

    Adição da tela de Splash

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
-import {AppRegistry, LogBox} from 'react-native';
+import {AppRegistry} from 'react-native';
 import Routes from './src/router';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Routes);
-LogBox.ignoreLogs(['Warning: Async Storage has been extracted from react-native core']); 
