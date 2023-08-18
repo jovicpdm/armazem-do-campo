b9a9d40f81bc81c6ac3486b1d28eda8a4b368876
alife-echo <alifesilva90@gmail.com>
2023-08-17 22:26:21 -0300

    my requests

---
+++ src/screens/MyRequests.js
@@ -0,0 +1,93 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, FlatList} from 'react-native';
+import {getDatabase, ref, onValue} from 'firebase/database';
+import MyOrdersCard from '../components/MyOrdersCard'
+import OrdersCard from '../components/OrdersCard';
+import TitleScreen from '../components/TitleScreen';
+import TopScreen from '../components/TopScreen';
+import WhiteAreaWithoutScrollView from '../components/WhiteAreaWithoutScrollView';
+import Logo from '../components/Logo';
+import GrayTextCenter from '../components/GrayTextCenter';
+
+
+
+
+export default function MyRequest ({navigation,route}) {
+    const db = getDatabase();
+    const [orders, setOrders] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const dbRef = ref(db, 'order'); 
+    const listOrders = async () => {
+        setLoading(true)
+        const dataArray = [];
+        await new Promise(resolve => {
+          onValue(dbRef, snapshot => {
+            snapshot.forEach(snap => {
+            let {idUser} = snap.val()
+              if(idUser === route.params.id){
+                const {date, codeNumber, formPay, requests,status,total,id} = snap.val();
+                let orders = {
+                  date,
+                  codeNumber,
+                  formPay,
+                  requests,
+                  status,
+                  total,
+                  id
+                };
+                dataArray.push(orders);
+            }    
+            });
+            resolve();
+          });
+        });
+        setOrders(dataArray); 
+        setLoading(false)   
+      };
+      
+      useEffect(() => {
+        setInterval(() => {listOrders()}, 1000);
+        return () => {
+          setOrders([]); 
+        };
+      }, [])
+        return(
+            <>
+            <Logo/>
+              <TopScreen>
+                <TitleScreen>Meus Pedidos</TitleScreen>
+              </TopScreen>
+              <WhiteAreaWithoutScrollView>
+        
+               {orders.length === 0 ? <GrayTextCenter>Você não tem pedidos</GrayTextCenter> : null}
+              
+                <FlatList
+                  data={orders}
+                  scrollEnabled
+                  keyExtractor={item => {
+                    return item.id;
+                  }}
+                  renderItem={({item}) => {
+                    
+                    return (
+                    <>
+                      <MyOrdersCard 
+                        requests={item.requests.products}
+                        date={item.date}
+                        codeNumber={item.codeNumber}
+                        formPay={item.formPay}
+                        status={item.status}
+                        total={item.total}
+                        id={item.id}
+                      />
+                      </>
+                    );
+                  }}
+                />
+              
+                
+              </WhiteAreaWithoutScrollView>
+            </>
+        )
+}
+
